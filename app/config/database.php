7946ed3b1597032c93ca4325a0d194738b12a3f4
ejaxon <eric@deepweave.com>
2014-08-12 15:08:35 -0400

    Set to use Postgresql

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => 'pgsql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -66,9 +66,9 @@ return array(
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'forge',
-			'username' => 'forge',
-			'password' => '',
+			'database' => 'cnp',
+			'username' => 'vagrant',
+			'password' => 'vagrant',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
