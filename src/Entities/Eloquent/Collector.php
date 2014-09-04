51ece9a7539a74e689f7da64d44bb79f1024d45f
ejaxon <eric@deepweave.com>
2014-09-03 20:28:57 -0400

    Added static method to compose Collector specifications and generate resulting spec

--- src/Entities/Eloquent/Collector.php
+++ src/Entities/Eloquent/Collector.php
@@ -11,4 +11,28 @@ class Collector extends \Eloquent {
 	 */
 	protected $table = 'collectors';
 
+	/**
+	 * Recursively read in and merge specs
+	 * @param  integer $id  ID of the end of the spec chain
+	 * @return StdClass    Merged specification object
+	 */
+	public static function getFullSpecification ($id)
+	{
+    	$collector = Collector::find($id);
+    	if ($collector) $spec = json_decode($collector->specification, true);
+    	if (array_key_exists('baseSpecificationId', $spec)) {
+    		$tmpspec = self::getFullSpecification($spec['baseSpecificationId']);
+    		if ( ! array_key_exists('input', $spec) && array_key_exists('input', $tmpspec)) {
+    			$spec['input'] = $tmpspec['input'];
+    		}
+    		if ( ! array_key_exists('elements', $spec) && array_key_exists('elements', $tmpspec)) {
+    			$spec['elements'] = $tmpspec['elements'];
+    		}
+    		if ( ! array_key_exists('relations', $spec) && array_key_exists('relations', $tmpspec)) {
+    			$spec['relations'] = $tmpspec['relations'];
+    		}
+    	}
+    	return $spec;
+	}
+
 }
