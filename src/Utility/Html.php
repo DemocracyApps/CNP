ee017effc0ec50991c43205d01697f6fc6adf6b4
ejaxon <eric@deepweave.com>
2014-09-07 18:44:58 -0400

    Moved output helper functions form autoinput.blade.php to a utility class.

---
+++ src/Utility/Html.php
@@ -0,0 +1,77 @@
+<?php
+namespace DemocracyApps\CNP\Utility;
+
+class Html {
+
+    private static $selfClosingElements = array(
+        'area' => true,
+        'base' => true,
+        'br' => true,
+        'col' => true,
+        'command' => true,
+        'embed' => true,
+        'hr' => true,
+        'img' => true,
+        'input' => true,
+        'keygen' => true,
+        'link' => true,
+        'meta' => true,
+        'param' => true,
+        'source' => true,
+        'track' => true,
+        'wbr' => true
+        );
+
+    static function createSelfClosingElement($type)
+    {
+        if ( ! array_key_exists($type, self::$selfClosingElements)) {
+            throw new \Exception("Attempt to create invalid self-closing HTML element " . $type);
+        }
+        echo "<".$type.">";
+    }
+
+    static function createElement($type, $content, $properties)
+    {
+        echo "<".$type;
+        foreach ($properties as $key => $value) {
+         echo " ". $key . "=\"".$value."\"";
+        }
+        if ($content) {
+         echo ">";
+         echo $content;
+         echo "</".$type.">";
+        }
+        else {
+        echo ">";
+        echo "</".$type.">";
+        }
+    }
+
+    static function startElement($type, $properties)
+    {
+        echo "<".$type;
+        foreach ($properties as $key => $value) {
+         echo " ". $key . "=\"".$value."\"";
+        }
+        echo ">";
+    }
+
+    static function endElement($type)
+    {
+        echo "</".$type.">";
+    }
+
+    static function createInput($desc)
+    {
+        self::startElement("div", array('class' => 'form-group'));
+        self::createElement("label", $desc['prompt'], array('for' => $desc['tag']));
+        if ($desc['type'] == 'text') {
+         self::createElement('input', null, array('class' => 'form-control', 'name' => $desc['tag'], 'type'=>'text'));
+        }
+        elseif ($desc['type'] == 'textarea') {
+         self::createElement('textarea', null, array('class' => 'form-control', 'name' => $desc['tag'],
+                       'cols'=>'50', 'rows' => '10'));
+        }
+        self::endElement("div");
+    }
+}
--- src/Views/stories/autoinput.blade.php
+++ src/Views/stories/autoinput.blade.php
@@ -3,58 +3,6 @@
 @section('content')
 <h1>Tell Your New Story</h1>
 
-<?php
-
-   function createElement($type, $content, $properties)
-   {
-      $nonSelfClosingElements = array('textarea'=>true);
-      echo "<".$type;
-      foreach ($properties as $key => $value) {
-         echo " ". $key . "=\"".$value."\"";
-      }
-      if ($content) {
-         echo ">";
-         echo $content;
-         echo "</".$type.">";
-      }
-      else {
-         if (array_key_exists($type, $nonSelfClosingElements)) {
-            echo ">";
-            echo "</".$type.">";
-         }
-         else {
-            echo "/>";
-         }
-      }
-   }
-   function startElement($type, $properties)
-   {
-      echo "<".$type;
-      foreach ($properties as $key => $value) {
-         echo " ". $key . "=\"".$value."\"";
-      }
-      echo ">";
-   }
-   function endElement($type)
-   {
-      echo "</".$type.">";
-   }
-
-   function createInput($desc)
-   {
-      startElement("div", array('class' => 'form-group'));
-      createElement("label", $desc['prompt'], array('for' => $desc['tag']));
-      if ($desc['type'] == 'text') {
-         createElement('input', null, array('class' => 'form-control', 'name' => $desc['tag'], 'type'=>'text'));
-      }
-      elseif ($desc['type'] == 'textarea') {
-         createElement('textarea', null, array('class' => 'form-control', 'name' => $desc['tag'],
-                       'cols'=>'50', 'rows' => '10'));
-      }
-      endElement("div");
-
-   }
-?>
 {{ Form::open(['route' => 'stories.store']) }}
    <input type="hidden" name="driver" value="{{$collector->getDriver()->id}}"/>
    <input type="hidden" name="collector" value="{{$collector->id}}"/>
@@ -67,8 +15,9 @@
             $done = true;
          else
             if (array_key_exists('prompt', $next)) {
-               createInput($next);
+               \DemocracyApps\CNP\Utility\Html::createInput($next);
             }
+         \DemocracyApps\CNP\Utility\Html::createSelfClosingElement('br');
 
          if ($count > 10) $done = true;
          ++$count;
