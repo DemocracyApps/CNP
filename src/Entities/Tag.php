194a6cd715cd24ba419187ef2b55cfd4c421b61b
ejaxon <eric@deepweave.com>
2014-09-08 12:35:56 -0400

    Added a findByName function - not sure yet whether to carry over to Denizen

--- src/Entities/Tag.php
+++ src/Entities/Tag.php
@@ -1,5 +1,6 @@
 <?php
 namespace DemocracyApps\CNP\Entities;
+use Illuminate\Support\Facades\DB as DB;
 
 class Tag extends Denizen 
 {
@@ -15,6 +16,18 @@ class Tag extends Denizen
         if (static::$classDenizenType < 0) {
             static::$classDenizenType = \CNP::getDenizenTypeId('Tag');
         }
+    }
+
+    static public function findByName($nm)
+    {
+        $data = DB::table(self::$tableName)
+                  ->where('name', $nm)->first();
+        $result = null;
+        if ($data != null) {
+            $result = new static($data->name, $data->userid);
+            self::fill($result, $data);
+        }
+        return $result;
     }
 
 }
