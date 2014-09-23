1796200662380b4eb9f5d67c8ceff2cc4d383a38
ejaxon <eric@deepweave.com>
2014-09-23 09:50:01 -0700

    Added a line to the Why Stories page

--- src/Views/demo/intro_stories.blade.php
+++ src/Views/demo/intro_stories.blade.php
@@ -8,6 +8,7 @@
 <ul>
     <li>Stories are the primary way we understand our world - what it is and what it could be</li>
     <li>Shared and interconnected stories are the foundation of our sense of community</li>
+    <li>Stories distill large, complex ideas into sticky, memorable pieces that can be carried and conveyed [<a href="http://itstartswith.com/2014/05/why-are-stories-so-important/" target="_blank">1</a>]</li>
     <li>Even the least sophisticated can convey rich information via stories</li>
     <li>Recent research on community planning indicates an important role for storytelling</li> 
   </ul>
