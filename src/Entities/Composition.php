0bd1f63dbcf08d60019ce2c9c2e9bfd33c4e8ac9
ejaxon <eric@deepweave.com>
2014-10-07 14:26:22 -0400

    Adding a 'Composition' element (the reflection of the Composition in the graph)

---
+++ src/Entities/Composition.php
@@ -0,0 +1,20 @@
+<?php
+namespace DemocracyApps\CNP\Entities;
+use Illuminate\Support\Facades\DB as DB;
+
+class Composition extends Element 
+{
+    static  $classElementType = -1;
+    protected $author;
+
+    function __construct ($nm = null, $userid = null) {
+        parent::__construct($nm, $userid, static::$classElementType);
+    }
+
+    static public function initialize() 
+    {
+        if (static::$classElementType < 0) {
+            static::$classElementType = \CNP::getElementTypeId('Tag');
+        }
+    }
+}
