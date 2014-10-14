ca3e594631631a40fc8eb419854df25159d7e5e3
ejaxon <eric@deepweave.com>
2014-10-14 07:21:58 -0400

    Added a multi-select input type

--- src/Compositions/Inputs/ComposerInputDriver.php
+++ src/Compositions/Inputs/ComposerInputDriver.php
@@ -112,6 +112,16 @@ class ComposerInputDriver extends \Eloquent {
             }
             Html::endElement('select');
         }
+        elseif ($desc['inputType'] == 'multiselect') {
+            Html::startElement('select', array('class' => 'form-control', 'name' => $desc['id'].'[]', 'multiple' => null));
+            $optionList = $desc['options'];
+            foreach ($optionList as $opt) {
+                $display = $opt['display'];
+                $value = $opt['value'];
+                Html::createElement('option', $display, array('value'=>$value));
+            }
+            Html::endElement('select');
+        }
         Html::endElement("div");
     }
 
