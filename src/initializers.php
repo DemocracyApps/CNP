b119e9a0eef5e760dc85d142eafac385267e3227
ejaxon <eric@deepweave.com>
2014-11-02 07:42:29 -0500

    Created a demo user that can be used without social network login

--- src/initializers.php
+++ src/initializers.php
@@ -5,21 +5,45 @@ use DemocracyApps\CNP\Entities\Eloquent\AppState;
 // Assumes that providers.php has already been called.
 
 // Check whether we need any DB initializations
-if (Schema::hasTable('app_state') && Schema::hasTable('relation_types')) {
+if (Schema::hasTable('app_state')) {
+	if (Schema::hasTable('relation_types')) {
 
-	$rtinit = AppState::where('name','=', 'relationTypesInitialized')->get()->first();
-	if (! $rtinit) {
-		\Log::info("Initializing relationTypes");
-		$fileName = base_path()."/src/Entities/entities.json";
-		$str = file_get_contents($fileName);
-		$str = json_minify($str);
-		$cfig = json_decode($str, true);
-		DemocracyApps\CNP\Entities\Eloquent\RelationType::initDB($cfig);
-		$rtinit = new DemocracyApps\CNP\Entities\Eloquent\AppState;
-		$rtinit->name = 'relationTypesInitialized';
-		$rtinit->value = '1';
-		$rtinit->save();
+		$rtinit = AppState::where('name','=', 'relationTypesInitialized')->get()->first();
+		if (! $rtinit) {
+			\Log::info("Initializing relationTypes");
+			$fileName = base_path()."/src/Entities/entities.json";
+			$str = file_get_contents($fileName);
+			$str = json_minify($str);
+			$cfig = json_decode($str, true);
+			DemocracyApps\CNP\Entities\Eloquent\RelationType::initDB($cfig);
+			$rtinit = new DemocracyApps\CNP\Entities\Eloquent\AppState;
+			$rtinit->name = 'relationTypesInitialized';
+			$rtinit->value = '1';
+			$rtinit->save();
+		}
 	}
+	if (Schema::hasTable('users')) {
+		$userInit = AppState::where('name','=','usersInitialized')->get()->first();
+		if (! $userInit) {
+            $user = new \DemocracyApps\CNP\Entities\Eloquent\User;
+            $user->name = "Demo User";
+            $user->save();
+            \Log::info("Created demo user with id " . $user->getId());
+            $person = new \DemocracyApps\CNP\Entities\Person($user->name, $user->getId());
+            $person->setContent($user->name);
+            $person->save();
+            \Log::info("Set person id of Demo User to" . $person->getId());
+
+            $user->elementid = $person->getId();
+            $user->save();
+
+			$userInit = new DemocracyApps\CNP\Entities\Eloquent\AppState;
+			$userInit->name = 'usersInitialized';
+			$userInit->value = '1';
+			$userInit->save();			
+		}
+	}
+
 }
 
 // We need to do some intializations on all the Element types
