2120e8a6327eb3cd24d37d4a57129e738e8b3a53
ejaxon <eric@deepweave.com>
2015-01-24 13:06:08 -0500

    Added a TableBackedObject class to do my own ORM more simply.

--- src/Entities/ProjectUser.php
+++ src/Entities/ProjectUser.php
@@ -1,73 +1,22 @@
 <?php namespace DemocracyApps\CNP\Entities;
 
 use Illuminate\Support\Facades\DB;
+use \DemocracyApps\CNP\Database\TableBackedObject;
 
 
-class ProjectUser {
+class ProjectUser extends TableBackedObject {
     static  $tableName = 'project_users';
+    static protected $tableFields = array('project', 'user', 'access',
+                                          'created_at', 'updated_at');
 
     public  $id = null;
     public  $project = -1;
     public  $user = -1;
     public  $access = -1;
 
-    public static function getTableName() {
-        return self::$tableName;
-    }
-
-    public function save()
-    {
-        if ($this->id == null) {
-            $this->id = DB::table(self::$tableName)->insertGetId(
-                array(
-                    'project' => $this->project,
-                    'user' => $this->user,
-                    'access' => $this->access,
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s')
-                )
-            );
-        }
-        else {
-            DB::table(self::$tableName)
-                ->where('id',$this->id)
-                ->update(
-                    array(
-                        'project' => $this->project,
-                        'user' => $this->user,
-                        'access' => $this->access,
-                        'updated_at' => date('Y-m-d H:i:s')
-                    )
-                );
-        }
-    }
-
-    protected static function fill ($instance, $data)
-    {
-        $instance->{'id'} = $data->id;
-        $instance->{'project'} = $data->project;
-        $instance->{'user'} = $data->user;
-        $instance->{'access'} = $data->level;
-    }
-
-    /**
-     * Find a element by its primary key
-     */
-    public static function find ($id)
-    {
-        $data = DB::table(self::$tableName)
-            ->where('id', $id)->first();
-        $result = null;
-        if ($data != null) {
-            $result = new static();
-            self::fill($result, $data);
-        }
-        return $result;
-    }
-
     public static function lookup($project, $user)
     {
-        return DB::table(self::$tableName)
+        return DB::table(static::$tableName)
             ->where ('project', $project) -> where('user', '=', $user)-> first();
     }
 
