0ccd2433fa45f8dc53b1c67da92fced53e493efe
ejaxon <eric@deepweave.com>
2015-02-02 14:27:10 -0500

    Fixed a bug (no clue when THAT got there).

---
+++ src/Analysis/Presentation/TopTagsPresentation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ericjackson
+ * Date: 2/2/15
+ * Time: 11:29 AM
+ */
+
+namespace DemocracyApps\CNP\Analysis\Presentation;
+
+
+use DemocracyApps\CNP\Presentation\PerspectivePresentation;
+
+class TopTagsPresentation extends PerspectivePresentation {
+
+}
