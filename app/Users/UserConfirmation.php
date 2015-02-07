f5c2cc6a7ebee4befae728e0afcd31914ae22771
ejaxon <eric@deepweave.com>
2015-02-07 17:44:11 -0500

    various

--- app/Users/UserConfirmation.php
+++ app/Users/UserConfirmation.php
@@ -3,10 +3,25 @@
 namespace DemocracyApps\CNP\Users;
 
 
-class UserConfirmation extends \Eloquent {
+use DemocracyApps\CNP\Utility\TableBackedObject;
+
+class UserConfirmation extends TableBackedObject {
+    static  $tableName = 'user_confirmations';
+    static protected $tableFields = array('user', 'type', 'code',
+        'expires', 'done',
+        'created_at', 'updated_at');
+
+    public  $id = null;
+    public  $user = null;
+    public  $type = null;
+    public  $code = null;
+    public  $expires = null;
+    public  $done = null;
+    public  $created_at = null;
+    public  $updated_at = null;
 
     public function initialize (User $user, $type, $hours) {
-        $this->user = $user->getId();
+        $this->user = $user->id;
         $this->type = $type;
         $this->expires = date('Y-m-d H:i:s', time() + $hours * 60 * 60);
         $this->code = uniqid($type . ".", true);
