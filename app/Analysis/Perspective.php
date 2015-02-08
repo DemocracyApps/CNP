474bbe35229293854f497522647ab3be8b0e4d1c
ejaxon <eric@deepweave.com>
2015-02-08 15:42:26 -0500

    Forgot this one.

---
+++ app/Analysis/Perspective.php
@@ -0,0 +1,50 @@
+<?php
+namespace DemocracyApps\CNP\Analysis;
+
+use DemocracyApps\CNP\Utility\TableBackedObject;
+
+
+class Perspective extends TableBackedObject {
+    static protected $tableName = 'perspectives';
+    static protected $tableFields = array('name', 'type', 'project', 'specification', 'description', 'requires_analysis',
+        'last', 'created_at', 'updated_at');
+
+    public $id;
+    public $name;
+    public $type;
+    public $project;
+    public $specification;
+    public $description;
+    public $requires_analysis;
+    public $last;
+    public $created_at;
+    public $updated_at;
+
+    public function getContent ()
+    {
+        $output = "<div class='perspective-main-div'>";
+        $presentationClassName = '\DemocracyApps\CNP\Analysis\Presentation\\' . $this->type . "Presentation";
+
+        if (class_exists($presentationClassName)) {
+            $cfig = null;
+            if ($this->specification != null) {
+                $str = json_minify($this->specification);
+                $cfig = json_decode($str, true);
+            }
+            $reflectionMethod = new \ReflectionMethod($presentationClassName, 'getContent');
+            $output .= $reflectionMethod->invokeArgs(null, array($this, $cfig, $this->last));
+        }
+        else {
+            throw new Exception("No presentation class defined for perspective " . $this->type);
+        }
+
+        $output .= "</div>";
+        //dd($output);
+        return $output;
+
+    }
+
+    public function getAnalysis() {
+        return AnalysisOutput::whereColumn('perspective', '=', $this->id);
+    }
+}
