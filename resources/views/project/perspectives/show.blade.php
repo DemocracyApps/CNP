160f6ee1b420bce6ebbdb5097bf29b84ea15fd97
ejaxon <eric@deepweave.com>
2015-02-15 12:22:28 -0500

    Moving forward on perspectives. Also cleaned up menu for loggedin vs loggedout

--- resources/views/project/perspectives/show.blade.php
+++ resources/views/project/perspectives/show.blade.php
@@ -3,10 +3,19 @@
 
 @section('content')
     <div class="row">
-        <h1>{!! $perspective->name !!}!</h1>
-        <p> {!! $perspective->description !!} </p>
-        <br>
+        <div class="col-md-9">
+            <h1>{!! $perspective->name !!}!</h1>
+        </div>
+        <div class="col-md-3">
+            <a href="/{!! $project->id!!}/perspectives">All Perspectives</a>
+        </div>
     </div>
+    <div class="row">
+        <div class="col-md-9">
+        <p> {!! $perspective->description !!} </p>
+        </div>
+    </div>
+    <br>
     <br>
     <div class="row">
 
